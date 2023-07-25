bc552c2e52f570f2b5f283593373d243b900687b
Felipe Nonato <105125624+felipe-nonato@users.noreply.github.com>
2023-07-25 22:34:07 +0000

    iniciando transicao para fases

--- web/src/pages/capacitacao.js
+++ web/src/pages/capacitacao.js
@@ -1,40 +1,19 @@
-import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Capacitacao.module.css';
-import stylesGlobal from '@/styles/Global.module.css';
-const inter = Inter({ subsets: ['latin'] });
-import { useRouter } from 'next/router';
-import SideBar from '@/components/sidebar';
-import Background from '@/components/background';
-import HeadPadrao from '@/components/headPadrao';
-import BarraPesquisa from '@/components/barraPesquisa';
-import Apresentacao from '@/components/apresentacao';
-import { useState } from 'react';
-
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Capacitacao.module.css";
+import stylesGlobal from "@/styles/Global.module.css";
+const inter = Inter({ subsets: ["latin"] });
+import { useRouter } from "next/router";
+import SideBar from "@/components/sidebar";
+import Background from "@/components/background";
+import HeadPadrao from "@/components/headPadrao";
+import BarraPesquisa from "@/components/barraPesquisa";
+import Apresentacao from "@/components/apresentacao";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const [mostrarVideos, setMostrarVideos] = useState(false);
-  const [mostrarDocumentos, setMostrarDocumentos] = useState(false);
-  const [mostrarMapasMentais, setMostrarMapasMentais] = useState(false);
-
-  const toggleMostrarVideos = () => {
-    setMostrarVideos(!mostrarVideos);
-    setMostrarDocumentos(false); // Certifique-se de desativar a exibição de documentos ao clicar no botão de vídeos
-    setMostrarMapasMentais(false); // Certifique-se de desativar a exibição de mapas mentais ao clicar no botão de vídeos
-  };
-
-  const toggleMostrarDocumentos = () => {
-    setMostrarDocumentos(!mostrarDocumentos);
-    setMostrarVideos(false); // Certifique-se de desativar a exibição de vídeos ao clicar no botão de documentos
-    setMostrarMapasMentais(false); // Certifique-se de desativar a exibição de mapas mentais ao clicar no botão de documentos
-  };
-
-  const toggleMostrarMapasMentais = () => {
-    setMostrarMapasMentais(!mostrarMapasMentais);
-    setMostrarVideos(false); // Certifique-se de desativar a exibição de vídeos ao clicar no botão de mapas mentais
-    setMostrarDocumentos(false); // Certifique-se de desativar a exibição de documentos ao clicar no botão de mapas mentais
-  };
+  const [fase, setFase] = useState(1);
 
   const styleBg = {
     backgroundImage: `url("/big.png")`,
@@ -43,43 +22,55 @@ export default function Home() {
 
   return (
     <>
-      <HeadPadrao  >Home</HeadPadrao >
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <HeadPadrao>Home</HeadPadrao>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       <Background>
         <div className={styles.all}>
           <BarraPesquisa></BarraPesquisa>
           <div className={styles.options}>
-            <div className={styles.option} 
+            <div
+              className={styles.option}
               style={{
-                backgroundColor: mostrarVideos ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.08)',
+                backgroundColor:
+                  fase == 1 ? "rgba(0, 0, 0, 0.2)" : "rgba(0, 0, 0, 0.08)",
               }}
-            onClick={toggleMostrarVideos}>
-              Videos
+              onClick={() => setFase(1)}
+            >
+              Fase 1
             </div>
-            <div className={styles.option} 
-            style={{
-              backgroundColor: mostrarDocumentos ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.08)',
-            }}
-            onClick={toggleMostrarDocumentos}>
-              Documentos
+            <div
+              className={styles.option}
+              style={{
+                backgroundColor:
+                  fase == 2 ? "rgba(0, 0, 0, 0.2)" : "rgba(0, 0, 0, 0.08)",
+              }}
+              onClick={() => setFase(2)}
+            >
+              Fase 2
             </div>
-            <div className={styles.option} 
-            style={{
-              backgroundColor: mostrarMapasMentais ? 'rgba(0, 0, 0, 0.2)' : 'rgba(0, 0, 0, 0.08)',
-            }}
-            onClick={toggleMostrarMapasMentais}>
-              Mapas Mentais
+            <div
+              className={styles.option}
+              style={{
+                backgroundColor:
+                  fase == 3 ? "rgba(0, 0, 0, 0.2)" : "rgba(0, 0, 0, 0.08)",
+              }}
+              onClick={() => setFase(3)}
+            >
+              Fase 3
             </div>
           </div>
 
-          {mostrarVideos && (
+          {fase == 1 && (
             <div className={styles.videos}>
               {/* Renderizar seus vídeos aqui */}
               {/* Exemplo de vídeo: */}
-              
+
               <div className={styles.videoItem}>
                 <iframe
-                 //width="400"
+                  //width="400"
                   //height="200"
                   src="https://www.youtube.com/embed/EDhkxMvpnEE"
                   title="YouTube video player"
@@ -100,38 +91,38 @@ export default function Home() {
                 ></iframe>
               </div>
               <div className={styles.videoItem}>
-                <iframe 
-              //width="560"
-               //height="315" 
-               src="https://www.youtube.com/embed/MGWOw9IEHco" 
-               title="YouTube video player" 
-               frameborder="0" 
-               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-               allowfullscreen>
-                </iframe>
+                <iframe
+                  //width="560"
+                  //height="315"
+                  src="https://www.youtube.com/embed/MGWOw9IEHco"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
               </div>
               <div className={styles.videoItem}>
-              <iframe 
-              width="560" 
-              height="315" 
-              src="https://www.youtube.com/embed/_u8qTN3cCnQ" 
-              title="YouTube video player" 
-              frameborder="0" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              allowfullscreen>
-              </iframe>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/_u8qTN3cCnQ"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
               </div>
             </div>
           )}
 
-          {mostrarDocumentos && (
+          {fase == 2 && (
             <div className={styles.documentos}>
               DOCUMENTOS
               {/* Renderizar seus documentos aqui */}
             </div>
           )}
 
-          {mostrarMapasMentais && (
+          {fase == 3 && (
             <div className={styles.mapasMentais}>
               MAPAS
               {/* Renderizar seus mapas mentais aqui */}
